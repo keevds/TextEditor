cfe39ea316ebfddaf2672390ce9ad4d25938b01c
Kevin Barbosa <kevinbbst@gmail.com>
2024-09-23 08:03:09 -0300

    Iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Crir novo arquivo");
+    Console.WriteLine("0 - Sair");
+
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0:
+            System.Environment.Exit(0);
+            break;
+        case 1:
+            Abrir();
+            break;
+        case 2:
+            Editar();
+            break;
+        default:
+            Menu();
+            break;
+    }
+
+    static void Abrir() { }
+
+    static void Editar() { }
+
+}
